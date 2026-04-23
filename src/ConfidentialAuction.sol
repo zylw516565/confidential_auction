6a80e1c77b8ca525fe4f7f020f1c66e3818205bf
zhangyinglei <zylw516565@163.com>
2026-04-23 17:05:50 +0800

    add bid function

--- src/ConfidentialAuction.sol
+++ src/ConfidentialAuction.sol
@@ -16,11 +16,11 @@ contract ConfidentialAuction is IConfidentialAuctionErrors, ReentrancyGuard{
     address seller;
     uint32  endOfBiddingPeriod;
     uint32  endOfRevealPeriod;
+    bool    started;
     uint32  count;
     //-------------------
-    uint64  topBid;
-    uint64  secondTopBid;
-    address topBidVault;
+    uint256  topBid;
+    uint256  secondTopBid;
     //-------------------
     bytes32 collateralizationDeadlineBlockHash;
   }
@@ -51,6 +51,11 @@ contract ConfidentialAuction is IConfidentialAuctionErrors, ReentrancyGuard{
       uint256 bidValue
   );
 
+  event Bidded(
+      address tokenContract,
+      uint256 tokenId
+  );
+
   // Emitted when the first bid is revealed for an auction.
   event CollateralizationDeadlineSet(
       address tokenContract,
@@ -59,23 +64,33 @@ contract ConfidentialAuction is IConfidentialAuctionErrors, ReentrancyGuard{
       uint256 deadlineBlockNumber
   );
 
+  struct BidInfo {
+    uint256 bidValue;
+    address tokenContract;
+    uint256 tokenId;
+  }
+
   // A mapping storing auction parameters and state, indexed by
   // the ERC721 contract address and token ID of the asset being
   // auctioned.
   mapping(address => mapping(uint256 => Auction)) public auctions_;
 
-  // A mapping storing whether or not the bid for a `ConfidentialVault` was revealed. 
-  mapping(address => bool) public revealedVaults_;
+  //The bids of all participants for a certain NTF
+  mapping(address => BidInfo) biddings_;
 
   function createAuction(
     address tokenContract,
     uint256 tokenId,
     uint32  bidPeriod,
     uint32  revealPeriod,
-    uint64 reservePrice
-  ) 
+    uint64  reservePrice
+  )
     external nonReentrant
   {
+    if(tokenContract == address(0)) {
+      revert InvalidTokenContractError();
+    }
+
     Auction storage auction = auctions_[tokenContract][tokenId];
 
     if (bidPeriod < 1 hours) {
@@ -99,12 +114,12 @@ contract ConfidentialAuction is IConfidentialAuctionErrors, ReentrancyGuard{
     auction.topBid = reservePrice;
     auction.secondTopBid = reservePrice;
     // Reset
-    auction.topBidVault = address(0);
     auction.collateralizationDeadlineBlockHash = bytes32(0);
 
     // The seller transfers the NFT assets to the current contract
     ERC721(tokenContract).transferFrom(msg.sender, address(this), tokenId);
 
+    auction.started = true;
     emit AuctionCreated(
       tokenContract,
       tokenId,
@@ -115,121 +130,170 @@ contract ConfidentialAuction is IConfidentialAuctionErrors, ReentrancyGuard{
     );
   }
 
-
-  function revealBid(
-      address tokenContract,
-      uint256 tokenId,
-      uint48 bidValue,
-      bytes32 salt,
-      CollateralizationProof calldata proof
+  /// @param tokenContract The address of the ERC721 contract for the asset
+  ///        being auctioned.
+  /// @param tokenId The ERC721 token ID of the asset being auctioned.
+  /// @param amount The value of the bid.
+  function bid(
+    address tokenContract,
+    uint256 tokenId,
+    uint256 amount
   )
-      external
-      nonReentrant
+    external payable nonReentrant
   {
+    if(tokenContract == address(0)) {
+      revert InvalidTokenContractError();
+    }
+
     Auction storage auction = auctions_[tokenContract][tokenId];
 
-    // The bidding for the auction hasn't started yet or the time for disclosing the bid has passed
-    if(
-      block.timestamp <= auction.endOfBiddingPeriod ||
-      block.timestamp >  auction.endOfRevealPeriod
+    if (
+      block.timestamp > auction.endOfBiddingPeriod ||
+      !auction.started
     ) {
-      revert NotInRevealPeriodError();
+      revert NotInBidPeriodError();
     }
 
-    uint32 auctionIndex = auction.count;
-    address vault = getVaultAddress(
-        tokenContract, 
-        tokenId, 
-        auctionIndex, 
-        msg.sender, 
-        bidValue, 
-        salt
+    if(amount <= 0) {
+      revert InvalidBidError(amount);
+    }
+
+    require(msg.sender.balance >= amount, InefficiencyBalanceError(msg.sender.balance, amount));
+    address(this).call{value: amount}("");
+
+    biddings_[msg.sender].bidValue = amount;
+    biddings_[msg.sender].tokenContract = tokenContract;
+    biddings_[msg.sender].tokenId = tokenId;
+
+    uint256 currentTopBid = auction.topBid;
+    if(amount > auction.topBid) {
+      auction.topBid = amount;
+      auction.secondTopBid = currentTopBid;
+    } else if (amount > auction.secondTopBid) {
+      auction.secondTopBid = amount;
+    }
+
+    emit Bidded(
+      tokenContract,
+      tokenId
     );
-
-    if(revealedVaults_[vault]) {
-      revert BidAlreadyRevealedError(vault);
-    }
-    revealedVaults_[vault] = true;
-
-    uint256 bidValueWei = bidValue * BASE_BID_UNIT;
-    bool isCollateralized = true;
-
-    // If this is the first bid revealed, record the block hash of the 
-    // previous block. All other bids must have been collateralized by 
-    // that block. 
-    if (auction.collateralizationDeadlineBlockHash == bytes32(0)) {
-      if (vault.balance < bidValueWei) {
-          // Deploy vault to return ETH to bidder
-          new ConfidentialVault{salt: salt}(
-              tokenContract, 
-              tokenId, 
-              auctionIndex, 
-              msg.sender,
-              bidValue
-          );
-          isCollateralized = false;
-      } else {
-          auction.collateralizationDeadlineBlockHash = blockhash(block.number - 1);
-          emit CollateralizationDeadlineSet(
-              tokenContract, 
-              tokenId, 
-              auctionIndex,
-              block.number - 1
-          );
-      }
-    } else {
-      // All other bidders must prove that their balance was 
-      // sufficiently collateralized by the deadline block.
-      uint256 vaultBalance = _getProvenAccountBalance(
-          proof.accountMerkleProof,
-          proof.blockHeaderRLP,
-          auction.collateralizationDeadlineBlockHash,
-          vault
-      );
-      if (vaultBalance < bidValueWei) {
-          // Deploy vault to return ETH to bidder
-          new ConfidentialVault{salt: salt}(
-              tokenContract, 
-              tokenId, 
-              auctionIndex, 
-              msg.sender,
-              bidValue
-          );
-          isCollateralized = false;
-      }
-    }
-
-    if (isCollateralized) {
-        // Update record of (second-)highest bid as necessary
-        uint64 currentTopBid = auction.topBid;
-        if (bidValue > currentTopBid) {
-            auction.topBid = bidValue;
-            auction.secondTopBid = currentTopBid;
-            auction.topBidVault = vault;
-        } else {
-            if (bidValue > auction.secondTopBid) {
-                auction.secondTopBid = bidValue;
-            }
-            // Deploy vault to return ETH to bidder
-            new ConfidentialVault{salt: salt}(
-                tokenContract, 
-                tokenId, 
-                auctionIndex, 
-                msg.sender,
-                bidValue
-            );
-        }
-
-        emit BidRevealed(
-            tokenContract,
-            tokenId,
-            vault,
-            msg.sender,
-            salt,
-            bidValueWei
-        );
-    }
   }
+
+
+  // function revealBid(
+  //     address tokenContract,
+  //     uint256 tokenId,
+  //     uint48 bidValue,
+  //     bytes32 salt,
+  //     CollateralizationProof calldata proof
+  // )
+  //     external
+  //     nonReentrant
+  // {
+  //   Auction storage auction = auctions_[tokenContract][tokenId];
+
+  //   // The bidding for the auction hasn't started yet or the time for disclosing the bid has passed
+  //   if(
+  //     block.timestamp <= auction.endOfBiddingPeriod ||
+  //     block.timestamp >  auction.endOfRevealPeriod
+  //   ) {
+  //     revert NotInRevealPeriodError();
+  //   }
+
+  //   uint32 auctionIndex = auction.count;
+  //   address vault = getVaultAddress(
+  //       tokenContract, 
+  //       tokenId, 
+  //       auctionIndex, 
+  //       msg.sender, 
+  //       bidValue, 
+  //       salt
+  //   );
+
+  //   if(revealedVaults_[vault]) {
+  //     revert BidAlreadyRevealedError(vault);
+  //   }
+  //   revealedVaults_[vault] = true;
+
+  //   uint256 bidValueWei = bidValue * BASE_BID_UNIT;
+  //   bool isCollateralized = true;
+
+  //   // If this is the first bid revealed, record the block hash of the 
+  //   // previous block. All other bids must have been collateralized by 
+  //   // that block. 
+  //   if (auction.collateralizationDeadlineBlockHash == bytes32(0)) {
+  //     if (vault.balance < bidValueWei) {
+  //         // Deploy vault to return ETH to bidder
+  //         new ConfidentialVault{salt: salt}(
+  //             tokenContract, 
+  //             tokenId, 
+  //             auctionIndex, 
+  //             msg.sender,
+  //             bidValue
+  //         );
+  //         isCollateralized = false;
+  //     } else {
+  //         auction.collateralizationDeadlineBlockHash = blockhash(block.number - 1);
+  //         emit CollateralizationDeadlineSet(
+  //             tokenContract, 
+  //             tokenId, 
+  //             auctionIndex,
+  //             block.number - 1
+  //         );
+  //     }
+  //   } else {
+  //     // All other bidders must prove that their balance was 
+  //     // sufficiently collateralized by the deadline block.
+  //     uint256 vaultBalance = _getProvenAccountBalance(
+  //         proof.accountMerkleProof,
+  //         proof.blockHeaderRLP,
+  //         auction.collateralizationDeadlineBlockHash,
+  //         vault
+  //     );
+  //     if (vaultBalance < bidValueWei) {
+  //         // Deploy vault to return ETH to bidder
+  //         new ConfidentialVault{salt: salt}(
+  //             tokenContract, 
+  //             tokenId, 
+  //             auctionIndex, 
+  //             msg.sender,
+  //             bidValue
+  //         );
+  //         isCollateralized = false;
+  //     }
+  //   }
+
+  //   if (isCollateralized) {
+  //       // Update record of (second-)highest bid as necessary
+  //       uint64 currentTopBid = auction.topBid;
+  //       if (bidValue > currentTopBid) {
+  //           auction.topBid = bidValue;
+  //           auction.secondTopBid = currentTopBid;
+  //           auction.topBidVault = vault;
+  //       } else {
+  //           if (bidValue > auction.secondTopBid) {
+  //               auction.secondTopBid = bidValue;
+  //           }
+  //           // Deploy vault to return ETH to bidder
+  //           new ConfidentialVault{salt: salt}(
+  //               tokenContract, 
+  //               tokenId, 
+  //               auctionIndex, 
+  //               msg.sender,
+  //               bidValue
+  //           );
+  //       }
+
+  //       emit BidRevealed(
+  //           tokenContract,
+  //           tokenId,
+  //           vault,
+  //           msg.sender,
+  //           salt,
+  //           bidValueWei
+  //       );
+  //   }
+  // }
 
   // Returns the seller for the most recent auction of the given asset.
   function getSeller(
@@ -247,24 +311,11 @@ contract ConfidentialAuction is IConfidentialAuctionErrors, ReentrancyGuard{
       address tokenContract,
       uint256 tokenId
   )
-      external
-      view
-      returns (uint256 bid)
+    external
+    view
+    returns (uint256 bidValue)
   {
       return auctions_[tokenContract][tokenId].secondTopBid * BASE_BID_UNIT;
-  }
-
-  // Returns vault address associated with the highest bid for the most 
-  // recent auction of the given asset.
-  function getHighestBidVault(
-      address tokenContract,
-      uint256 tokenId
-  )
-      external
-      view
-      returns (address vault)
-  {
-      return auctions_[tokenContract][tokenId].topBidVault;
   }
 
   // Gets the parameters and state of an auction in storage.
--- src/IConfidentialAuctionErrors.sol
+++ src/IConfidentialAuctionErrors.sol
@@ -7,9 +7,13 @@ interface IConfidentialAuctionErrors {
     error InvalidAuctionIndexError(uint32 index);
     error BidPeriodTooShortError(uint32 bidPeriod);
     error RevealPeriodTooShortError(uint32 revealPeriod);
+    error NotInBidPeriodError();
     error NotInRevealPeriodError();
     error IncorrectVaultAddressError(address expectedVault, address actualVault);
     error UnrevealedBidError();
     error CannotWithdrawError();
     error BidAlreadyRevealedError(address vault);
+    error InvalidTokenContractError();
+    error InvalidBidError(uint256 bid);
+    error InefficiencyBalanceError(uint256 balance, uint256 amount);
 }
